551c25dab98fb98232074a5203c1b554cb2ec407
Paolo Musolino <info@codeido.com>
2017-06-14 17:51:38 +0200

    Generated Demo Project

---
+++ PMSuperButtonSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PMSuperButtonSample
+//
+//  Created by Paolo Musolino on 14/06/17.
+//  Copyright © 2017 PMSuperButton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
