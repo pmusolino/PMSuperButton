9286235c1b023ed7d08f5e0a98e56962f53b566b
Paolo Musolino <info@codeido.com>
2017-06-16 18:55:08 +0200

    Added thirdButton in sample project

--- PMSuperButtonSample/ViewController.swift
+++ PMSuperButtonSample/ViewController.swift
@@ -25,6 +25,12 @@ class ViewController: UIViewController {
             self.firstButton.hideLoader()
         }
         
+        let thirdButton = PMSuperButton(frame: CGRect(x: 100, y: 400, width: 120, height: 30))
+        thirdButton.setTitle("Try this feature", for: .normal)
+        thirdButton.touchUpInside { 
+            thirdButton.showLoader()
+        }
+        view.addSubview(thirdButton)
     }
     
     override func viewDidAppear(_ animated: Bool) {
