53cfc8d660d9050dc6049069648b2c0cf4e4be09
SakhabaevE <sakhabaevegor@gmail.com>
2019-11-17 23:06:57 +0700

    - fixed path

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .target(
             name: "PMSuperButton-iOS",
-            path: "PMSuperButton-iOS"
+            path: "PMSuperButton"
         )
     ]
 )
